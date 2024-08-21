ed806d060485edb362026e3c2c1f2f68106b5c81
venkatesh-987 <151707210+venkatesh-987@users.noreply.github.com>
2024-08-21 16:17:31 +0530

    Add files via upload

---
+++ argument.python.py
@@ -0,0 +1,12 @@
+a=("SASI","COLLEGE","ENGINEERING")
+b=["SASI","COLLEGE","ENGINEERING"]
+c={"SASI":1,"COLLEGE":2,"ENGINEERING":3}
+d="HELLO WORLD"
+e=10.23
+f=11.22
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
