5a78c340a48b8340176d8d529ab10bba18c83c11
venkatesh-987 <151707210+venkatesh-987@users.noreply.github.com>
2024-08-21 16:17:12 +0530

    Add files via upload

---
+++ data type.python.py
@@ -0,0 +1,6 @@
+a=100
+print("the type of variable having value",a,"is",type(a))
+b=10.2345
+print("the type of variable having value",b,"is",type(b))
+c=100+3
+print("the type of variable having",c,"is",type(c))
