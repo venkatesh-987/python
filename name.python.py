cd667a03de2ba0e75b69463bc57b952414c1c726
venkatesh-987 <151707210+venkatesh-987@users.noreply.github.com>
2024-08-21 16:20:04 +0530

    Add files via upload

---
+++ name.python.py
@@ -0,0 +1,6 @@
+n=int(input("enter the number that you want to display your name:"))
+name=input("Enter name:")
+while n>=0:
+          print(name)
+          n=n-1
+          print("end of the program")
