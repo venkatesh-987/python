5284e7e5e02a0a62b46eace2be16efaf86d9820b
venkatesh-987 <151707210+venkatesh-987@users.noreply.github.com>
2024-08-21 16:22:45 +0530

    Add files via upload

---
+++ even.python.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number"))
+if n%2==0:
+    printt(n,"is even number")
+    print("end of if block")
+else:
+    print(n,"is odd number")
+    print("end of else block")
+print("End of the program")
+print("Excution is completed")
